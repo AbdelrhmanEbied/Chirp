a22c54a262f4e2a81b10f63ff6f925b791cc1d2c
Abdelrhman Ebied <abdelrhmanebied170@gmail.com>
2026-09-18 12:48:33 +0300

    fix: k6 install method - download GPG key directly

--- deploy.sh
+++ deploy.sh
@@ -30,8 +30,7 @@ fi
 
 if ! command -v k6 &>/dev/null; then
   log "Installing k6"
-  sudo gpg -k
-  sudo gpg --no-default-keyring --keyring /usr/share/keyrings/k6-archive-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D68
+  curl -sS https://dl.k6.io/key.gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/k6-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/k6-archive-keyring.gpg] https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
   sudo apt-get update -qq && sudo apt-get install -y -qq k6
 fi
