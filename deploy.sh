253f81f4da0f72be576faa96cd88222d978d073e
Abdelrhman Ebied <abdelrhmanebied170@gmail.com>
2026-09-19 16:42:54 +0300

    fix: use --force-replace instead of deprecated --force for helm

--- deploy.sh
+++ deploy.sh
@@ -180,7 +180,7 @@ helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-contro
   --set clusterName="$CLUSTER" \
   --set serviceAccount.create=true \
   --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"="" \
-  --force \
+  --force-replace \
   --wait
 
 log "Step 10: Building and pushing Docker images"
